8b1e2747b5db0ae6ddfb8036fc7c0e0ff8108a1d
Simon Attfield <simattfield@gmail.com>
2016-04-15 16:58:23 +0000

    List of contacts implemented

--- js/app.js
+++ js/app.js
@@ -9,14 +9,27 @@ function newContact(name, email, phone, address) {
 	this.email = email;
 }
 
+var displayContactNames = function() {
+    for( var i = 0; i < addressBook.contact.length; i++) {
+        $('#contactList').append('<li>' + addressBook.contact[i].name + '</li>');
+    } 
+};
 
 var newContactInformation = document.querySelector('#contactForm');
 
 newContactInformation.addEventListener('submit', function(event) {
     event.preventDefault();
+    console.log("Hello");
     var contact = new newContact(this[0].value, 
                                  this[1].value, 
                                  this[2].value, 
                                  [this[3].value, this[4].value, this[5].value, this[6].value]);
     addressBook.contact.push(contact);
-})
+    displayContactNames();
+});
+
+
+
+
+
+
